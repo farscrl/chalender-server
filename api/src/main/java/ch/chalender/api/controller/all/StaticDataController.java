6d58dbb431dee3379032a414feb3485009b59ec7
Gion-Andri Cantieni <gion-andri@gmx.ch>
2023-08-07 15:43:28 +0200

    feat: expose static data for form

---
+++ api/src/main/java/ch/chalender/api/controller/all/StaticDataController.java
@@ -0,0 +1,38 @@
+package ch.chalender.api.controller.all;
+
+import ch.chalender.api.model.EventGenre;
+import ch.chalender.api.model.EventRegion;
+import ch.chalender.api.repository.EventGenresRepository;
+import ch.chalender.api.repository.EventRegionsRepository;
+import io.swagger.v3.oas.annotations.tags.Tag;
+import lombok.extern.slf4j.Slf4j;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.http.ResponseEntity;
+import org.springframework.web.bind.annotation.*;
+
+import java.util.List;
+
+@Slf4j
+@RestController
+@RequestMapping("/api/static")
+@Tag(name = "Static", description = "Static data")
+public class StaticDataController {
+
+    @Autowired
+    private EventGenresRepository eventGenresRepository;
+
+    @Autowired
+    private EventRegionsRepository eventRegionsRepository;
+
+    @GetMapping("/genres")
+    public ResponseEntity<List<EventGenre>> listGenres() {
+        List<EventGenre> genres = eventGenresRepository.findByIsHiddenIsFalse(false);
+        return ResponseEntity.ok(genres);
+    }
+
+    @GetMapping("/regions")
+    public ResponseEntity<List<EventRegion>> listRegions() {
+        List<EventRegion> regions = eventRegionsRepository.findByIsHiddenIsFalse(false);
+        return ResponseEntity.ok(regions);
+    }
+}
--- api/src/main/java/ch/chalender/api/model/EventRegion.java
+++ api/src/main/java/ch/chalender/api/model/EventRegion.java
@@ -1,11 +1,15 @@
 package ch.chalender.api.model;
 
+import com.fasterxml.jackson.annotation.JsonIgnore;
 import lombok.Data;
 import org.springframework.data.mongodb.core.mapping.Document;
 
 @Document("regions")
 @Data
 public class EventRegion {
-    private String id;
+    private int id;
     private String name;
+
+    @JsonIgnore
+    private boolean isHidden = false;
 }
