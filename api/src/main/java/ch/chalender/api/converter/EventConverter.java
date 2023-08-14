befd2a63c8143c5028d31ce0e5da91dad98cdb41
Gion-Andri Cantieni <gion-andri@gmx.ch>
2023-08-14 17:11:38 +0200

    feat: use event object to create new event
    minor changes

--- api/src/main/java/ch/chalender/api/converter/EventConverter.java
+++ api/src/main/java/ch/chalender/api/converter/EventConverter.java
@@ -6,6 +6,7 @@ import ch.chalender.api.model.EventVersion;
 import org.modelmapper.ModelMapper;
 
 import java.util.List;
+import java.util.Objects;
 import java.util.stream.Collectors;
 
 public class EventConverter {
@@ -18,17 +19,34 @@ public class EventConverter {
         return event;
     }
 
-    public static EventDto toEventDto(ModelMapper modelMapper, Event event) {
-        EventVersion eventVersion = event.getDraft();
+    public static EventDto toEventDto(ModelMapper modelMapper, Event event, EventVersionSelection eventVersionSelection) {
+        EventVersion eventVersion = null;
+        if (eventVersionSelection == EventVersionSelection.CURRENTLY_PUBLISHED) {
+            eventVersion = event.getCurrentlyPublished();
+        } else if (eventVersionSelection == EventVersionSelection.DRAFT) {
+            eventVersion = event.getDraft();
+        } else if (eventVersionSelection == EventVersionSelection.LAST_REVIEWED) {
+            eventVersion = event.getLastReviewed();
+        }
+        if (eventVersion == null) {
+            return null;
+        }
         EventDto eventDto = modelMapper.map(eventVersion, EventDto.class);
         eventDto.setId(event.getId());
 
         return eventDto;
     }
 
-    public static List<EventDto> toEventDtoList(ModelMapper modelMapper, List<Event> events) {
+    public static List<EventDto> toEventDtoList(ModelMapper modelMapper, List<Event> events, EventVersionSelection eventVersionSelection) {
         return events.stream()
-                .map(event -> toEventDto(modelMapper, event))
+                .map(event -> toEventDto(modelMapper, event, eventVersionSelection))
+                .filter(Objects::nonNull)
                 .collect(Collectors.toList());
+    }
+
+    public enum EventVersionSelection {
+        CURRENTLY_PUBLISHED,
+        DRAFT,
+        LAST_REVIEWED
     }
 }
