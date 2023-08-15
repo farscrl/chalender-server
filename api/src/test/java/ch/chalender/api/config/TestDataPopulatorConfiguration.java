4a23723d8f6ebb6d3b1be868514f20bb830681df
Gion-Andri Cantieni <gion-andri@gmx.ch>
2023-08-15 13:37:33 +0200

    test: run tests in testcontainers instead of embedmongo

--- api/src/test/java/ch/chalender/api/config/TestDataPopulatorConfiguration.java
+++ api/src/test/java/ch/chalender/api/config/TestDataPopulatorConfiguration.java
@@ -1,17 +1,20 @@
 package ch.chalender.api.config;
 
+import ch.chalender.api.repository.EventsRepository;
 import org.springframework.context.annotation.Bean;
 import org.springframework.core.io.ClassPathResource;
 import org.springframework.core.io.Resource;
+import org.springframework.data.mongodb.MongoDatabaseFactory;
+import org.springframework.data.mongodb.MongoTransactionManager;
 import org.springframework.data.repository.init.Jackson2RepositoryPopulatorFactoryBean;
 
 public class TestDataPopulatorConfiguration {
     @Bean
-    public Jackson2RepositoryPopulatorFactoryBean getRepositoryPopulator() {
+    public Jackson2RepositoryPopulatorFactoryBean getRepositoryPopulator(EventsRepository eventsRepository) {
+        eventsRepository.deleteAll();
         Jackson2RepositoryPopulatorFactoryBean factory = new Jackson2RepositoryPopulatorFactoryBean();
         factory.setResources(new Resource[]{new ClassPathResource("testdata/events.json")});
 
         return factory;
     }
-
 }
--- api/src/test/java/ch/chalender/api/repository/EventsRepositoryTest.java
+++ api/src/test/java/ch/chalender/api/repository/EventsRepositoryTest.java
@@ -1,33 +1,24 @@
 package ch.chalender.api.repository;
 
+import ch.chalender.api.base.MongoDbBaseTest;
 import ch.chalender.api.config.TestDataPopulatorConfiguration;
-import org.junit.jupiter.api.AfterEach;
 import org.junit.jupiter.api.Test;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.boot.test.autoconfigure.data.mongo.DataMongoTest;
 import org.springframework.context.annotation.Import;
-import org.springframework.data.mongodb.core.MongoTemplate;
+import org.springframework.transaction.annotation.Transactional;
 
 import static org.junit.jupiter.api.Assertions.assertEquals;
 
-@DataMongoTest
-@Import(TestDataPopulatorConfiguration.class)
-public class EventsRepositoryTest {
 
-    @Autowired
-    private MongoTemplate mongoTemplate;
+public class EventsRepositoryTest extends MongoDbBaseTest {
 
     @Autowired
     private EventsRepository eventsRepository;
 
 
     @Test
-    void findByLocationWithin_FindsRestaurantsWithinAGivenDistance() {
+    void findAllEventsInRepo() {
         assertEquals(2, eventsRepository.findAll().size());
-    }
-
-    @AfterEach
-    void cleanUpDatabase() {
-        mongoTemplate.getDb().drop();
     }
 }
