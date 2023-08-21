3ac4d653e389c55ead6ea8b41082aad064f012ec
Gion-Andri Cantieni <gion-andri@gmx.ch>
2023-08-21 09:11:30 +0200

    test: run mongo tests in testcontainer db

--- api/src/test/java/ch/chalender/api/base/MongoDbBaseTest.java
+++ api/src/test/java/ch/chalender/api/base/MongoDbBaseTest.java
@@ -1,20 +1,41 @@
 package ch.chalender.api.base;
 
-import ch.chalender.api.controller.admin.FixturesController;
+import ch.chalender.api.service.FixturesService;
 import org.junit.jupiter.api.BeforeAll;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.boot.test.autoconfigure.web.servlet.AutoConfigureMockMvc;
 import org.springframework.boot.test.context.SpringBootTest;
+import org.springframework.test.context.DynamicPropertyRegistry;
+import org.springframework.test.context.DynamicPropertySource;
+import org.testcontainers.containers.GenericContainer;
+import org.testcontainers.containers.wait.strategy.Wait;
+import org.testcontainers.junit.jupiter.Testcontainers;
 
 import java.io.IOException;
 
 @SpringBootTest
 @AutoConfigureMockMvc
+@Testcontainers
 public class MongoDbBaseTest {
+
+    public static GenericContainer mongoDbContainer = new GenericContainer("mongo:5.0.2")
+            .withExposedPorts(27017)
+            .waitingFor(Wait.forLogMessage(".*Waiting for connections.*\\n", 1))
+            .withEnv("MONGO_INITDB_ROOT_USERNAME", "root")
+            .withEnv("MONGO_INITDB_ROOT_PASSWORD", "root");
+
+    @DynamicPropertySource
+    static void mongoDbProperties(DynamicPropertyRegistry registry) {
+        mongoDbContainer.start();
+        registry.add("spring.data.mongodb.host", mongoDbContainer::getHost);
+        registry.add("spring.data.mongodb.port", mongoDbContainer::getFirstMappedPort);
+        registry.add("spring.data.mongodb.database", () -> "chalender");
+        registry.add("spring.data.mongodb.username", () -> "root");
+        registry.add("spring.data.mongodb.password", () -> "root");
+    }
 
     @BeforeAll
     public static void beforeAll(@Autowired FixturesService fixturesService) throws IOException {
-        //mongoDbContainer.start();
         System.out.println("Loading fixtures for test...");
         fixturesService.loadEventGenreFixtures();
         fixturesService.loadEventRegionFixtures();
